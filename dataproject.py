7ae8deeeecbcf51b9a867077ce0f15513af4cc66
Bharath-varma-p <50948937+Bharath-varma-p@users.noreply.github.com>
2021-10-07 14:35:25 +0530

    2nd commit

--- dataproject.py
+++ dataproject.py
@@ -1,6 +1,6 @@
 import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
+# working with sample data.update
 import numpy as np
 import pandas as pd
 import yfinance as yf
